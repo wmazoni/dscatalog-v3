3c506fdb2cf911955cf824d89e5acfde2c247961
Wellington Mazoni <wmazoni@protonmail.com>
2022-04-10 19:31:00 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = categoryService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
