67521ad93a93737ec1fd5626b36c5166985adeb9
Wellington Mazoni <wmazoni@protonmail.com>
2022-04-15 10:47:14 -0300

    category dto

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDto.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+public class CategoryDto {
+
+    private Long id;
+    private String name;
+
+    public CategoryDto() {
+    }
+
+    public CategoryDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDto(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDto;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +17,8 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDto> findAll() {
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(CategoryDto::new).collect(Collectors.toList());
     }
 }
