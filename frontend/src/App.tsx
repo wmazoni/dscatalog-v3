54de6df0aad5c47ddae4752acdb9b9a55b6a01fc
Wellington Mazoni <wmazoni@protonmail.com>
2022-09-21 10:17:56 -0300

    create components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import './assets/styles/custom.scss';
+import 'assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
 
+import Routes from 'Routes';
 
-const App = () => {
-  return <Home />
-};
+function App() {
+  return (
+    <Routes />
+  );
+}
 
 export default App;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,29 +1,32 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
-import Navbar from 'components/Navbar';
+import { Link } from 'react-router-dom';
+
 import './styles.css';
+
 const Home = () => {
   return (
-    <>
-      <Navbar />
-      <div className="home-container">
-        <div className="home-card">
-          <div className="home-content-container">
-            <div>
-              <h1>Conheça o melhor catálogo de produtos</h1>
-              <p>
-                Ajudaremos você a encontrar os melhores produtos disponíveis no
-                mercado.
-              </p>
-            </div>
-            <ButtonIcon />
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado.
+            </p>
           </div>
-          <div className="home-image-container">
-            <MainImage />
+          <div>
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
           </div>
         </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
