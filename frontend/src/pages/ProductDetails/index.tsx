29c2bca3283309aa24196750e127cc66700ee4a8
Wellington <wmazoni@gmail.com>
2023-06-25 13:26:19 -0300

    useEffect, useStare, useParams

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,30 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './styles.css';
+import { useState, useEffect } from 'react';
+import { Product } from 'types/product';
+import axios from 'axios';
+import { BASE_URL } from 'util/request';
+
+type UrlParams = {
+  productId: string
+}
 
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then(response => {
+      setProduct(response.data)
+    })
+  }, [productId])
+  
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -17,21 +38,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               ></img>
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero,
-                sint.
+                {product?.description}
               </p>
             </div>
           </div>
