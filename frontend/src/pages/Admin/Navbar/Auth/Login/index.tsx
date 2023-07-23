537fb5dbeb3dd0ee532f7ea5a3cc2b5024eb2168
Wellington Mazoni <wmazoni@gmail.com>
2023-07-23 13:13:46 -0300

    Manual error handling

--- frontend/src/pages/Admin/Navbar/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Navbar/Auth/Login/index.tsx
@@ -3,63 +3,71 @@ import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import './styles.css';
 import { requestBackendLogin } from 'util/request';
+import { useState } from 'react';
 
 type FormData = {
-    username: string
-    password: string
-}
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        setHasError(false);
+        console.log('SUCESSO', response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
+  };
 
-    const { register, handleSubmit } = useForm<FormData>();
-    const onSubmit = (formData : FormData) => {
-        requestBackendLogin(formData)
-        .then(response => {
-            console.log('SUCESSO', response)
-        })
-        .catch(error => {
-            console.log('ERRO', error)
-        })
-        
-    };
-
-    return (
-        <div className="base-card login-card">
-            <h1>LOGIN</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <input
-                        {...register("username")}
-                        type="text"
-                        className="form-control base-input"
-                        placeholder="Email"
-                        name="username"
-                    />
-                </div>
-                <div className="mb-2">
-                    <input
-                        {...register("password")}
-                        type="password"
-                        className="form-control base-input "
-                        placeholder="Password"
-                        name="password"
-                    />
-                </div>
-                <Link to="/admin/auth/recover" className="login-link-recover">
-                    Esqueci a senha
-                </Link>
-                <div className="login-submit">
-                    <ButtonIcon text="Fazer login" />
-                </div>
-                <div className="signup-container">
-                    <span className="not-registered">Não tem Cadastro?</span>
-                    <Link to="/admin/auth/register" className="login-link-register">
-                        CADASTRAR
-                    </Link>
-                </div>
-            </form>
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">
+          Ocorreu um erro no preenchimento
         </div>
-    );
+      )}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register('username')}
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            {...register('password')}
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
