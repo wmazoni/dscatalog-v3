a5f2508d2ef5e45df2f96face0266a2c8f0da642
Wellington Mazoni <wmazoni@gmail.com>
2023-07-24 18:56:56 -0300

    form validation

--- frontend/src/pages/Admin/Navbar/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Navbar/Auth/Login/index.tsx
@@ -12,7 +12,7 @@ type FormData = {
 
 const Login = () => {
   const [hasError, setHasError] = useState(false);
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
@@ -37,21 +37,31 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i,
+                message: 'Email inválido'
+              }
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">{errors.username?.message}</div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password', {
+              required: 'Campo obrigatório'
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
