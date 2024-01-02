f50e56269b370528e0dafb95c0c16c42bbdcb9c5
Wellington Mazoni <well@wmazoni.com.br>
2024-01-02 11:47:41 -0300

    Check user authenticated

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -1,6 +1,15 @@
 import qs from 'qs'
 import axios, { AxiosRequestConfig } from 'axios'
 import history from './history'
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+    exp: number,
+    user_name: string,
+    authorities: Role[]
+}
 
 type LoginResponse = {
     "access_token": string,
@@ -64,3 +73,16 @@ axios.interceptors.response.use(function (response) {
     }
     return Promise.reject(error);
   });
+
+  export const getTokenData = () : TokenData | undefined => {
+    try {
+        return jwtDecode(getAuthData().access_token);
+    } catch (error) {
+        return undefined;
+    }
+  }
+
+  export const isAuthenticated = () : boolean => {
+    const tokenData = getTokenData();
+    return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+  }
