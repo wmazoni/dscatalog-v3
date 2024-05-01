22d33b421f2357194974cca343731dbbc8f198b7
Wellington Mazoni de Andrade <well@wmazoni.com.br>
2024-05-01 19:04:14 -0300

    UI restriction based on user roles

--- frontend/src/pages/Admin/Navbar/index.tsx
+++ frontend/src/pages/Admin/Navbar/index.tsx
@@ -1,25 +1,28 @@
 import { NavLink } from 'react-router-dom';
-import './styles.css'
+import './styles.css';
+import { hasAnyRoles } from 'util/requests';
 
 const Navbar = () => {
   return (
-    <nav className='admin-nav-container'>
+    <nav className="admin-nav-container">
       <ul>
         <li>
-          <NavLink to='/admin/products' className='admin-nav-item'>
+          <NavLink to="/admin/products" className="admin-nav-item">
             <p>Produtos</p>
           </NavLink>
         </li>
         <li>
-          <NavLink to='/admin/categories' className='admin-nav-item'>
+          <NavLink to="/admin/categories" className="admin-nav-item">
             <p>Categorias</p>
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/admin/users' className='admin-nav-item'>
-            <p>Usuários</p>
-          </NavLink>
-        </li>
+        {hasAnyRoles(['ROLE_ADMIN']) && (
+          <li>
+            <NavLink to="/admin/users" className="admin-nav-item">
+              <p>Usuários</p>
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
