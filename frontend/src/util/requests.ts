df83c2a73469a9a58e0426f811ade51220ae36b2
Wellington Mazoni de Andrade <well@wmazoni.com.br>
2024-05-01 18:54:12 -0300

    hasAnyRole

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -90,3 +90,15 @@ axios.interceptors.response.use(function (response) {
     const tokenData = getTokenData();
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
   }
+
+  export const hasAnyRoles = (roles: Role[]) : boolean => {
+    if (roles.length === 0) {
+        return true;
+    }
+    const tokenData = getTokenData();
+
+    if (tokenData !== undefined) {
+        return roles.some(role => tokenData.authorities.includes(role))
+    }
+    return false;
+  }
